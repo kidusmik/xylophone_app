3b3813e55242dbb44b3d3657c6ee4d847e812e2a
kidusmik <kidusmik@gmail.com>
2021-07-23 00:09:13 +0300

    Added audio player dependency/package. Implemented the audio player package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,87 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.pink.shade300),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note1.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.red.shade900),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note2.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.blue.shade900),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note3.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.yellow),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note4.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.grey),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note5.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.green),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note6.wav');
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.purple),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('assets_note7.wav');
+                },
+                child: Text(''),
+              ),
+            ],
+          ),
         ),
       ),
     );
